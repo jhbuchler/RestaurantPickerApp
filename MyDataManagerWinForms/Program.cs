8c4f4ba108c8352a8a7d969093d569e95a3f365c
Dillon J. Choi <dillonchoi1992@gmail.com>
2022-02-24 09:10:23 -0500

    fixed thread lock bug

--- MyDataManagerWinForms/Program.cs
+++ MyDataManagerWinForms/Program.cs
@@ -1,6 +1,6 @@
 namespace MyDataManagerWinForms
 {
-    internal static class Program
+    public static class Program
     {
         /// <summary>
         ///  The main entry point for the application.
@@ -11,7 +11,15 @@ namespace MyDataManagerWinForms
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm());
+
+            try
+            {
+                Application.Run(new MainForm());
+            }
+            catch (Exception ex)
+            {
+                throw;  
+            }
         }
     }
 }
